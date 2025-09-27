b6783d7d2561f7163da3724710a5a377803fab0e
hadibutt3560 <hadibutt3560@gmail.com>
2025-09-27 12:55:02 +0200

    chore: initial sentinels CRA scaffold

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Header() {
+return (
+<header className="site-header">
+<div className="container">
+<h1>Sentinels</h1>
+<p className="tagline">Watchful UI for your app</p>
+</div>
+</header>
+);
+}
